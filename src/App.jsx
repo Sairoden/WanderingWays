7c0122f8489f1d2b0c3f25bc09b2783d6b47d44d
Sairoden <odingandarosa@gmail.com>
2023-08-06 13:30:26 +0800

    navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // React
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 // Pages
@@ -48,10 +48,7 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
@@ -71,4 +68,4 @@ function App() {
 
 export default App;
 
-// 14 ka na
+// 17 ka na
